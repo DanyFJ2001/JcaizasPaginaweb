7c1282aa968a06284b31aaf2f74f7974b5baa562
DanyFJ2001 <danyjoel345@gmail.com>
2025-05-10 20:15:15 -0500

    Push 4 beta

--- src/app/components/autos/autos.component.ts
+++ src/app/components/autos/autos.component.ts
@@ -20,7 +20,7 @@ featuredCars = [
       location: 'Quito',
       price: 14700,
       negotiable: true,
-      image: 'https://i.ibb.co/c2Q4vJJ/renault-sandero.jpg',
+      image: '',
       isTop: true
     },
     {
@@ -32,7 +32,7 @@ featuredCars = [
       location: 'Quito',
       price: 18900,
       negotiable: true,
-      image: 'https://i.ibb.co/tqd6sNJ/nissan-versa.jpg',
+      image: '',
       isTop: true
     },
     {
@@ -44,7 +44,7 @@ featuredCars = [
       location: 'Quito',
       price: 7500,
       negotiable: true,
-      image: 'https://i.ibb.co/BybwFd7/lifan-620.jpg',
+      image: '',
       isTop: true
     },
     {
@@ -56,7 +56,7 @@ featuredCars = [
       location: 'Quito',
       price: 25000,
       negotiable: true,
-      image: 'https://i.ibb.co/F7M30LZ/chevrolet-dmax.jpg',
+      image: '',
       isTop: true
     },
     {
@@ -68,7 +68,7 @@ featuredCars = [
       location: 'Quito',
       price: 13500,
       negotiable: true,
-      image: 'https://i.ibb.co/bRDhbL7/chevrolet-grand-vitara.jpg',
+      image: '',
       isTop: true
     },
     {
@@ -80,33 +80,10 @@ featuredCars = [
       location: 'Quito',
       price: 6500,
       negotiable: true,
-      image: 'https://i.ibb.co/b5QyTL7/kia-optima.jpg',
+      image: '',
       isTop: true
     },
-    {
-      id: 7,
-      brand: 'Skoda',
-      model: 'Kodiaq',
-      year: 2018,
-      kilometers: 130000,
-      location: 'Samborondón',
-      price: 24000,
-      negotiable: true,
-      image: 'https://i.ibb.co/X8Jgv4V/skoda-kodiaq.jpg',
-      isTop: true
-    },
-    {
-      id: 8,
-      brand: 'Renault',
-      model: 'Duster',
-      year: 2025,
-      kilometers: 24000,
-      location: 'Quito',
-      price: 19500,
-      negotiable: true,
-      image: 'https://i.ibb.co/D4Q6S3X/renault-duster.jpg',
-      isTop: true
-    }
+   
   ];
 
   // Método para manejar el clic en el botón de oferta
